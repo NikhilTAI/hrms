0b66e24fc8bb8ab24f8b8c087f1b72aad612be41
Nikhil <nik.theappideas@gmail.com>
2021-09-24 16:50:39 +0530

    cors 7

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var app = express()
 
 const cors = require('cors');
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: true
 }));
