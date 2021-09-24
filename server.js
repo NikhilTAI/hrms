7c7b733deabd1fab1ee04ee9677df4b580dd859b
Nikhil <nik.theappideas@gmail.com>
2021-09-24 14:47:50 +0530

    cors stackoverflow

--- server.js
+++ server.js
@@ -18,11 +18,18 @@ db.once('open', function() {
 // init app
 var app = express()
 
-app.use(
-    cors({
-        origin: "*",
-    })
-);
+// app.use(
+//     cors({
+//         origin: "*",
+//     })
+// );
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // bodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
